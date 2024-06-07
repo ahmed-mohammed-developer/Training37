9f119b02e0aa0ff121a59a769dc62867da6fa012
ahmed <ahmed.mohammed.developer@gmail.com>
2024-06-07 09:04:32 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+
+function exponent_squared(number) {
+  // write your code here
+  return Math.pow(number, 2)
+}
+
+console.log(exponent_squared(-8))
+
+
+
+function circumference_of_rhombus(length) {
+  // write your code here
+    return length * 4;
+}
+console.log(circumference_of_rhombus(3.3))
+
